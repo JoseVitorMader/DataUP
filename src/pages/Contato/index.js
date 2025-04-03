f0e0fe7999966d67e0c76544e0eacbdef35eb327
JoseVitorMader <maderjosevitor@gmail.com>
2025-04-03 09:44:29 -0300

    Adicção das telas Sobre e contato

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,15 +1,38 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { FaInstagram, FaEnvelope } from 'react-icons/fa';
+import './style.css';
 
-function Contato(){
-  return(
-    <div>
-      <h1>Pagina Contato</h1>
-      <span>Contato da empresa (dd) xxxx-xxxx</span> <br/> <br/>
+function Contato() {
+  return (
+    <div className="contato-content">
+      <h1>Sobre</h1>
+      <p className="contato-text">
+        Se você deseja entrar em contato com o <strong>Tá Na Medida</strong>, temos o maior prazer em atender suas dúvidas, 
+        ouvir suas sugestões ou estabelecer parcerias! 📩💬
+        <br />
+        <br />
+        Seja qual for o meio, estamos ansiosos para falar com você e garantir que sua experiência com o <strong>Tá Na Medida</strong> seja incrível! 💚
+      </p>
 
-      <Link to="/">Home</Link> <br/>
-      <Link to="/sobre">Sobre</Link>
+      <h3>Entre em contato:</h3>
+      <div className="contato-links">
+        <a 
+          className="contato-button" 
+          href="https://www.instagram.com/ta.na.medida?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <FaInstagram /> Instagram
+        </a>
+        <a 
+          className="contato-button" 
+          href="mailto:tanamedidacedup@gmail.com" 
+        >
+          <FaEnvelope /> Email
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contato;
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,12 +1,26 @@
-import { Link } from 'react-router-dom';
+import './style.css';
+import React from 'react';
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 function Sobre() {
   return (
-    <div>
-      <h1>Pagina Sobre a empresa</h1> <br/> <br/> 
-
-      <Link to="/https://sites.google.com/estudante.sed.sc.gov.br/tanamedida/início?authuser=2">Pagina Home</Link> <br/>
-      <a href="https://sites.google.com/estudante.sed.sc.gov.br/tanamedida/início?authuser=2"><button>teste</button></a>
+    <div className="sobre-content">
+      <h1>Sobre</h1>
+      <p className="sobre-text">
+        O Ta na medida é um aplicativo cujo objetivo é promover a sustentabilidade no ambiente escolar, 
+        reduzindo os desperdícios na preparação de refeições por meio de uma plataforma que auxilia e orienta 
+        os profissionais da cozinha quanto às quantidades adequadas. O aplicativo também conta com uma aba 
+        dedicada à realização do ato de compostagem, apoiada por uma inteligência artificial para a gestão de alimentos.
+      </p>
+      <h3>Para mais informações acesse:</h3>
+      <a 
+        className="sobre-link" 
+        href="https://sites.google.com/estudante.sed.sc.gov.br/tanamedida/in%C3%ADcio?authuser=2" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        Site de Apoio <FaExternalLinkAlt />
+      </a>
     </div>
   );
 }
